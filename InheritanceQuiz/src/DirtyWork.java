c99d85013851bf2743779c67f5a08a764cd705c1
AndrewOrdonez <andrewordonez5@gmail.com>
2016-10-13 21:01:03 -0600

    Update DirtyWork.java

--- InheritanceQuiz/src/DirtyWork.java
+++ InheritanceQuiz/src/DirtyWork.java
@@ -6,66 +6,89 @@ public class DirtyWork
 public static void question(int i,int j){
 	q=i;
 	r=j;
-	System.out.println("if '");
+	System.out.print("if '");
 	switch (i){
-		case 0: System.out.println("Mountain m = new Mountain();");
+		case 0: System.out.print("Mountain m = new Mountain();");
 		break;
-		case 1:System.out.println("Mountain m = new Volcano();");
+		case 1:System.out.print("Mountain m = new Volcano();");
 		break;
-		case 2:System.out.println("Volcano v= new Mountain();");
+		case 2:System.out.print("Volcano m= new Mountain();");
 		break;
-		case 3:System.out.println("Volcano v= new Volcano();");
+		case 3:System.out.print("Volcano m= new Volcano();");
 		break;
 	}
-	System.out.println("' And '");
-	if (i<2){
+	System.out.print("' And '");
 		switch(j){
-			case 0: System.out.println("m.erode();");
+			case 0: System.out.print("m.erode();");
 			break;
-			case 1:System.out.println("m.obtainTourists();");
+			case 1:System.out.print("m.obtainTourists();");
 			break;
-			case 2:System.out.println("m.explode();");
+			case 2:System.out.print("m.explode();");
 			break;
 		}
-	}
-	else{
-		switch(j){
-		case 0: System.out.println("v.erode();");
-		break;
-		case 1:System.out.println("v.obtainTourists();");
-		break;
-		case 2:System.out.println("v.explode();");
-		break;
-		}
-	}
-	System.out.println("' Is called, what will be printed out?");
-	System.out.println("a:'The mountain erodes over time'\nb:'KABOOM!'\nc:'People come to see the views'\nd:'People come tosee the lava'\ne:the compiler throws an error");
+	
+
+	System.out.println("' are called, what will be printed out?");
+	System.out.println("a:'The mountain erodes over time'\nb:'KABOOM!'\nc:'People come to see the views'\nd:'People come to see the lava'\ne:the compiler throws an error");
 }
 public static boolean checkQuestion(char ans){
 	if (ans>96){
 		ans=(char) (ans-32);
 	}
 	if (q==2){
-		
+		if (ans==69){
+			return true;
+		}
+		else if (q==0&&r==2){
+			return true;
+		}
+		else {return false;}
 	}
 	switch(ans){
 		case 65:if (r==0){
 			return true;
 		}
-		else 
-			break;
-		case 66:
-			break;
-		case 67:
-			break;
-		case 68:
-			break;
-		case 69:if (q==2){
+		else {return false;}
+	case 66:if((q==1||q==3)&&r==2){return true;}
+	else {return false;}
+		case 67:if (r==1&&q==0){
 			return true;
 		}
-			break;
+		else {return false;}
+		case 68:if(r==1&&(q==1||q==3)){
+			return true;
+		}
+		else {return false;}
+		
 		default: System.out.println("You did not give a valid answer");
 		return false;
 	}
 }
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
